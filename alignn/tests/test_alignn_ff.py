e80fc856c22ecc760968a07f49d01e891fa13285
Brian DeCost <brian.decost@nist.gov>
2022-06-02 14:14:31 -0400

    add test for force gradient computation -- currently this triggers a segfault with dgl>=0.7.2

--- alignn/tests/test_alignn_ff.py
+++ alignn/tests/test_alignn_ff.py
@@ -1,6 +1,49 @@
-from jarvis.db.figshare import get_jid_data
+"""Testing code for force-field related modules."""
+from pathlib import Path
+
+import pandas as pd
+import torch
 from jarvis.core.atoms import Atoms as JAtoms
-from alignn.alignn_ff import ALIGNNFF_Calculator, RunMD, OptimizeAtoms
+from jarvis.db.figshare import get_jid_data
+from torch.utils.data import DataLoader
+
+from alignn.alignn_ff import ALIGNNFF_Calculator, OptimizeAtoms, RunMD
+from alignn.dataset import AtomisticConfigurationDataset
+from alignn.models.alignn_atomwise import ALIGNNAtomWise, ALIGNNAtomWiseConfig
+
+# load example force-field data to share across tests
+example_data = Path("alignn/examples/sample_data")
+dataset = AtomisticConfigurationDataset(
+    pd.read_json(example_data / "id_prop.json"),
+    line_graph=True,
+)
+
+collate = dataset.collate_line_graph
+dl = DataLoader(dataset, collate_fn=collate, batch_size=4)
+
+
+def test_force_grad():
+    """Test force gradient computation.
+
+    This triggers a segmentation fault in dgl>=0.7.2
+    """
+    model_cfg = ALIGNNAtomWiseConfig(
+        name="alignn_atomwise",
+        alignn_layers=2,
+        gcn_layers=2,
+        atom_input_features=1,
+        calculate_gradient=True,
+    )
+    model = ALIGNNAtomWise(model_cfg)
+
+    g, lg, t = next(iter(dl))
+    out = model((g, lg))
+
+    loss = torch.abs(out["total_energy"] - t["total_energy"]).sum()
+
+    # should not segfault
+    loss.backward()
+
 
 """
 def test_alignnff():
