fix total energy prediction and force calculation; clean up / simplify

alignn/models/alignn_atomwise.py
@@ -3,16 +3,19 @@
 A prototype crystal line graph network dgl implementation.
 """
 from typing import Tuple, Union
-from torch.autograd import grad
+
 import dgl
 import dgl.function as fn
 import numpy as np
-from dgl.nn import AvgPooling
 import torch
+from dgl.nn import AvgPooling, SumPooling
+
 # from dgl.nn.functional import edge_softmax
 from pydantic.typing import Literal
 from torch import nn
+from torch.autograd import grad
 from torch.nn import functional as F
+
 from alignn.models.utils import RBFExpansion
 from alignn.utils import BaseSettings
 
@@ -20,7 +23,7 @@ from alignn.utils import BaseSettings
 class ALIGNNAtomWiseConfig(BaseSettings):
     """Hyperparameter schema for jarvisdgl.models.alignn."""
 
-    name: Literal["alignn_atomwise"]
+    name: Literal["alignn_atomwise"] = "alignn_atomwise"
     alignn_layers: int = 4
     gcn_layers: int = 4
     atom_input_features: int = 92
@@ -28,21 +31,9 @@ class ALIGNNAtomWiseConfig(BaseSettings):
     triplet_input_features: int = 40
     embedding_features: int = 64
     hidden_features: int = 256
-    # fc_layers: int = 1
-    # fc_features: int = 64
     output_features: int = 1
-    grad_multiplier: int = -1
     calculate_gradient: bool = True
-    atomwise_output_features: int = 3
-    graphwise_weight: float = 1.0
-    gradwise_weight: float = 0.0
-    stresswise_weight: float = 0.0
-    atomwise_weight: float = 0.0
-    # if link == log, apply `exp` to final outputs
-    # to constrain predictions to be positive
-    link: Literal["identity", "log", "logit"] = "identity"
-    zero_inflated: bool = False
-    classification: bool = False
+    calculate_stress: bool = False
 
     class Config:
         """Configure model settings behavior."""
@@ -204,13 +195,8 @@ class ALIGNNAtomWise(nn.Module):
     ):
         """Initialize class with number of input features, conv layers."""
         super().__init__()
-        # print(config)
-        self.classification = config.classification
         self.config = config
-        if self.config.gradwise_weight == 0:
-            self.config.calculate_gradient = False
-        if self.config.atomwise_weight == 0:
-            self.config.atomwise_output_features = None
+
         self.atom_embedding = MLPLayer(
             config.atom_input_features, config.hidden_features
         )
@@ -252,32 +238,13 @@ class ALIGNNAtomWise(nn.Module):
             ]
         )
 
-        self.readout = AvgPooling()
-        if config.atomwise_output_features is not None:
-            self.fc_atomwise = nn.Linear(
-                config.hidden_features, config.atomwise_output_features
-            )
+        self.readout = SumPooling()
 
-        if self.classification:
-            self.fc = nn.Linear(config.hidden_features, 2)
-            self.softmax = nn.LogSoftmax(dim=1)
-        else:
-            self.fc = nn.Linear(config.hidden_features, config.output_features)
-        self.link = None
-        self.link_name = config.link
-        if config.link == "identity":
-            self.link = lambda x: x
-        elif config.link == "log":
-            self.link = torch.exp
-            avg_gap = 0.7  # magic number -- average bandgap in dft_3d
-            self.fc.bias.data = torch.tensor(
-                np.log(avg_gap), dtype=torch.float
-            )
-        elif config.link == "logit":
-            self.link = torch.sigmoid
+        self.fc = nn.Linear(config.hidden_features, config.output_features)
 
     def forward(
-        self, g: Union[Tuple[dgl.DGLGraph, dgl.DGLGraph], dgl.DGLGraph]
+        self,
+        g: Union[Tuple[dgl.DGLGraph, dgl.DGLGraph], dgl.DGLGraph],
     ):
         """ALIGNN : start with `atom_features`.
 
@@ -293,7 +260,6 @@ class ALIGNNAtomWise(nn.Module):
             z = self.angle_embedding(lg.edata.pop("h"))
 
         g = g.local_var()
-        result = {}
 
         # initial node features: atom feature network...
         x = g.ndata.pop("atom_features")
@@ -312,42 +278,45 @@ class ALIGNNAtomWise(nn.Module):
         # gated GCN updates: update node, edge features
         for gcn_layer in self.gcn_layers:
             x, y = gcn_layer(g, x, y)
-        # norm-activation-pool-classify
-        out = torch.empty(1)
-        if self.config.output_features is not None:
-            h = self.readout(g, x)
-            out = self.fc(h)
-            out = torch.squeeze(out)
-        atomwise_pred = torch.empty(1)
-        if self.config.atomwise_output_features is not None:
-            atomwise_pred = self.fc_atomwise(x)
-            # atomwise_pred = torch.squeeze(self.readout(g, atomwise_pred))
-        gradient = torch.empty(1)
+
+        # predict per-atom energy contribution
+        atomwise_energy = self.fc(x)
+
+        # total energy prediction
+        total_energy = torch.squeeze(self.readout(g, atomwise_energy))
+
+        forces = torch.empty(1)
         stress = torch.empty(1)
 
         if self.config.calculate_gradient:
+            # potentially we only need to build the computational graph
+            # for the forces at training time, so that we can compute
+            # the gradient of the force (and stress) loss?
             create_graph = True
-            # if config.normalize_graph_level_loss
-            # print ('out',out)
-            # print ('x',len(x))
 
-            # tmp_out = out*len(x)
-            # print ('tmp_out',tmp_out)
-            dy = (
-                self.config.grad_multiplier
-                * grad(
-                    # tmp_out,
-                    out,
-                    r,
-                    grad_outputs=torch.ones_like(out),
-                    create_graph=create_graph,
-                    retain_graph=True,
-                )[0]
+            # energy gradient contribution of each bond
+            # dU/dr
+            dy_dr = grad(
+                total_energy,
+                r,
+                grad_outputs=torch.ones_like(total_energy),
+                create_graph=create_graph,
+                retain_graph=True,
+            )[0]
+
+            # forces: negative energy gradient -dU/dr
+            pairwise_forces = -dy_dr
+
+            # reduce over bonds to get forces on each atom
+            g.edata["pairwise_forces"] = pairwise_forces
+            g.update_all(
+                fn.copy_e("pairwise_forces", "m"), fn.sum("m", "forces")
             )
-            g.edata["dy_dr"] = dy
-            g.update_all(fn.copy_e("dy_dr", "m"), fn.sum("m", "gradient"))
-            gradient = torch.squeeze(g.ndata["gradient"])
-            if self.config.stresswise_weight != 0:
+            forces = torch.squeeze(g.ndata["forces"])
+
+            if self.config.calculate_stress:
+                # make this a custom DGL aggregation?
+
                 # Under development, use with caution
                 # 1 eV/Angstrom3 = 160.21766208 GPa
                 # 1 GPa = 10 kbar
@@ -355,7 +324,7 @@ class ALIGNNAtomWise(nn.Module):
                 # Save volume as g.gdta['V']?
                 stress = -1 * (
                     160.21766208
-                    * torch.matmul(r.T, dy)
+                    * torch.matmul(r.T, dy_dr)
                     / (2 * g.ndata["V"][0])
                 )
                 # virial = (
@@ -364,13 +333,12 @@ class ALIGNNAtomWise(nn.Module):
                 #    * torch.einsum("ij, ik->jk", result["r"], result["dy_dr"])
                 #    / 2
                 # )  # / ( g.ndata["V"][0])
-        if self.link:
-            out = self.link(out)
 
-        if self.classification:
-            out = self.softmax(out)
-        result["out"] = out
-        result["grad"] = gradient
-        result["stress"] = stress
-        result["atomwise_pred"] = atomwise_pred
+        result = dict(
+            total_energy=total_energy,
+            forces=forces,
+            stress=stress,
+            atomwise_energy=atomwise_energy,
+        )
+
         return result