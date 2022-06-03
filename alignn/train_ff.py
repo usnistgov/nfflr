1521a5155758d1b20acac6ea4816d341dff8bfc4
Brian DeCost <brian.decost@nist.gov>
2022-06-03 16:06:53 -0400

    add performance metric and output logging

--- alignn/train_ff.py
+++ alignn/train_ff.py
@@ -1,16 +1,16 @@
 """Prototype training code for force field models."""
-from typing import Any, Callable
 
+import matplotlib.pyplot as plt
 import pandas as pd
 import torch
 from ignite.contrib.handlers.tqdm_logger import ProgressBar
 from ignite.engine import (
-    Engine,
     Events,
     create_supervised_evaluator,
     create_supervised_trainer,
 )
 from ignite.handlers import Checkpoint, DiskSaver, TerminateOnNan
+from ignite.handlers.stores import EpochOutputStore
 from ignite.metrics import Accuracy, Loss, MeanAbsoluteError
 from torch import nn
 from torch.utils.data import DataLoader
@@ -18,44 +18,158 @@ from torch.utils.data import DataLoader
 from alignn.config import TrainingConfig
 from alignn.dataset import AtomisticConfigurationDataset
 from alignn.models.alignn_atomwise import ALIGNNAtomWise, ALIGNNAtomWiseConfig
-from alignn.train import group_decay, setup_optimizer
+from alignn.training_utils import (
+    group_decay,
+    setup_evaluator_with_grad,
+    setup_optimizer,
+)
 
-# def setup_training(config, model):
-#     params = group_decay(model)
-#     optimizer = setup_optimizer(params, config)
+device = "cpu"
+if torch.cuda.is_available():
+    device = torch.device("cuda")
 
 
-def setup_evaluator(
-    model,
-    metrics,
-    prepare_batch,
-    device="cpu",
-    non_blocking=False,
-    output_transform: Callable[[Any, Any, Any], Any] = lambda x, y, y_pred: (
-        y_pred,
-        y,
-    ),
-):
-    """Set up custom ignite evaluation engine.
+def setup_scheduler(config, optimizer, steps_per_epoch):
+    """Configure OneCycle scheduler."""
+    pct_start = config.warmup_steps / (config.epochs * steps_per_epoch)
+    pct_start = min(pct_start, 0.3)
+    scheduler = torch.optim.lr_scheduler.OneCycleLR(
+        optimizer,
+        max_lr=config.learning_rate,
+        epochs=config.epochs,
+        steps_per_epoch=steps_per_epoch,
+        pct_start=pct_start,
+    )
 
-    Closely follows
-    https://github.com/pytorch/ignite/blob/master/ignite/engine/__init__.py#L558
-    with the exception of keeping gradient computation
+    return scheduler
+
+
+def select_target(name: str):
+    """Build ignite metric transforms for multi-output models.
+
+    `output` should be Tuple[Dict[str,torch.Tensor], Dict[str,torch.Tensor]]
     """
 
-    def evaluation_step(engine, batch):
-        """Evaluate step, including gradient tracing."""
-        model.eval()
-        x, y = prepare_batch(batch, device=device, non_blocking=non_blocking)
-        y_pred = model(x)
+    def output_transform(output):
+        """Select output tensor for metric computation."""
+        pred, target = output
+        return pred[name], target[name]
 
-        return output_transform(x, y, y_pred)
+    return output_transform
 
-    evaluator = Engine(evaluation_step)
-    for name, metric in metrics.items():
-        metric.attach(evaluator, name)
 
-    return evaluator
+def parity_plots(output, epoch, directory):
+    """Plot predictions for energy and forces."""
+    fig, axes = plt.subplots(ncols=2, figsize=(16, 8))
+    for batch in output:
+        pred, tgt = batch
+
+        axes[0].scatter(
+            tgt["total_energy"].detach().numpy(),
+            pred["total_energy"].detach().numpy(),
+            color="k",
+        )
+        axes[1].scatter(
+            tgt["forces"].detach().numpy(),
+            pred["forces"].detach().numpy(),
+            color="k",
+        )
+
+    plt.tight_layout()
+    plt.savefig(Path(directory) / f"parity_plots_{epoch:03d}.png")
+    plt.clf()
+    plt.close()
+
+
+def train_ff(config, model, dataloader):
+    """Train force field model."""
+    prepare_batch = dataloader.dataset.prepare_batch
+
+    params = group_decay(model)
+    optimizer = setup_optimizer(params, cfg)
+    scheduler = setup_scheduler(cfg, optimizer, len(dl))
+
+    criteria = {"total_energy": nn.MSELoss(), "forces": nn.MSELoss()}
+
+    def ff_criterion(outputs, targets):
+        """Specify combined energy and force loss."""
+        energy_loss = criteria["total_energy"](
+            outputs["total_energy"], targets["total_energy"]
+        )
+
+        # scale the forces before the loss
+        force_scale = 0.001
+        force_loss = criteria["forces"](
+            force_scale * outputs["forces"], force_scale * targets["forces"]
+        )
+
+        return energy_loss + force_loss
+
+    metrics = {
+        "loss": Loss(ff_criterion),
+        "mae_energy": MeanAbsoluteError(select_target("total_energy")),
+        "mae_forces": MeanAbsoluteError(select_target("forces")),
+    }
+
+    trainer = create_supervised_trainer(
+        model,
+        optimizer,
+        ff_criterion,
+        prepare_batch=prepare_batch,
+        device=device,
+    )
+
+    # create_supervised_evaluator
+    train_evaluator = setup_evaluator_with_grad(
+        model,
+        metrics=metrics,
+        prepare_batch=prepare_batch,
+        device=device,
+    )
+
+    # ignite event handlers:
+    trainer.add_event_handler(Events.EPOCH_COMPLETED, TerminateOnNan())
+
+    # apply learning rate scheduler
+    trainer.add_event_handler(
+        Events.ITERATION_COMPLETED, lambda engine: scheduler.step()
+    )
+
+    pbar = ProgressBar()
+    pbar.attach(trainer, output_transform=lambda x: {"loss": x})
+
+    # model checkpointing
+    to_save = dict(
+        model=model, optimizer=optimizer, scheduler=scheduler, trainer=trainer
+    )
+
+    handler = Checkpoint(
+        to_save,
+        DiskSaver(config.output_dir, create_dir=True, require_empty=False),
+        n_saved=1,
+        global_step_transform=lambda *_: trainer.state.epoch,
+    )
+    trainer.add_event_handler(Events.EPOCH_COMPLETED, handler)
+
+    # save outputs
+    # list of outputs for each batch
+    # in this case List[Tuple[Dict[str,torch.Tensor], Dict[str,torch.Tensor]]]
+    eos = EpochOutputStore()
+    eos.attach(train_evaluator, "output")
+
+    @trainer.on(Events.EPOCH_COMPLETED)
+    def log_training_results(engine):
+        """Log training results."""
+        train_evaluator.run(dataloader)
+        m = train_evaluator.state.metrics
+        epoch, loss = engine.state.epoch, metrics["loss"]
+        print(f"Training Results - Epoch: {epoch}  Avg loss: {loss:.2f}")
+        print(f"energy: {m['mae_energy']:.2f}  force: {m['mae_forces']:.4f}")
+
+        parity_plots(train_evaluator.state.output, epoch, config.output_dir)
+
+    # train the model!
+    trainer.run(dataloader, max_epochs=cfg.epochs)
 
 
 if __name__ == "__main__":
@@ -71,10 +185,14 @@ if __name__ == "__main__":
         atom_input_features=1,
         calculate_gradient=True,
     )
-    cfg = TrainingConfig(model=model_cfg, num_workers=0, epochs=10)
+    cfg = TrainingConfig(
+        model=model_cfg, num_workers=0, epochs=10, output_dir="./temp"
+    )
     print(cfg)
 
     model = ALIGNNAtomWise(model_cfg)
+    model.fc.bias.data = torch.tensor([-3.0])
+    model.to(device)
 
     lg = True
     dataset = AtomisticConfigurationDataset(
@@ -84,77 +202,5 @@ if __name__ == "__main__":
     dl = DataLoader(
         dataset, collate_fn=dataset.collate, batch_size=16, drop_last=True
     )
-    prepare_batch = dl.dataset.prepare_batch
 
-    params = group_decay(model)
-    optimizer = setup_optimizer(params, cfg)
-
-    steps_per_epoch = len(dl)
-    pct_start = cfg.warmup_steps / (cfg.epochs * steps_per_epoch)
-    pct_start = min(pct_start, 0.3)
-    scheduler = torch.optim.lr_scheduler.OneCycleLR(
-        optimizer,
-        max_lr=cfg.learning_rate,
-        epochs=cfg.epochs,
-        steps_per_epoch=steps_per_epoch,
-        pct_start=pct_start,
-    )
-
-    criteria = {"total_energy": nn.MSELoss(), "forces": nn.MSELoss()}
-
-    def ff_criterion(outputs, targets):
-        """Specify combined energy and force loss."""
-        energy_loss = criteria["total_energy"](
-            outputs["total_energy"], targets["total_energy"]
-        )
-
-        force_loss = criteria["forces"](outputs["forces"], targets["forces"])
-
-        return energy_loss + force_loss
-
-    metrics = {
-        "loss": Loss(ff_criterion),
-        # "mae": MeanAbsoluteError()
-    }
-
-    trainer = create_supervised_trainer(
-        model,
-        optimizer,
-        ff_criterion,
-        prepare_batch=prepare_batch,
-        # device=device,
-    )
-
-    # create_supervised_evaluator(
-    train_evaluator = setup_evaluator(
-        model,
-        metrics=metrics,
-        prepare_batch=prepare_batch,
-        # device=device,
-    )
-
-    # train_evaluator.add_event_handler(
-    #     Events.ITERATION_STARTED, lambda _: torch.set_grad_enabled(True)
-    # )
-
-    # ignite event handlers:
-    trainer.add_event_handler(Events.EPOCH_COMPLETED, TerminateOnNan())
-
-    # apply learning rate scheduler
-    trainer.add_event_handler(
-        Events.ITERATION_COMPLETED, lambda engine: scheduler.step()
-    )
-
-    pbar = ProgressBar()
-    pbar.attach(trainer, output_transform=lambda x: {"loss": x})
-
-    @trainer.on(Events.EPOCH_COMPLETED)
-    def log_training_results(trainer):
-        """Log training results."""
-        train_evaluator.run(dl)
-        metrics = train_evaluator.state.metrics
-        epoch, loss = trainer.state.epoch, metrics["loss"]
-        print(f"Training Results - Epoch: {epoch}  Avg loss: {loss:.2f}")
-
-    # train the model!
-    trainer.run(dl, max_epochs=cfg.epochs)
+    train_ff(cfg, model, dl)
---
+++ alignn/training_utils.py
@@ -0,0 +1,74 @@
+"""Common training setup utility functions."""
+from typing import Any, Callable
+
+import torch
+from ignite.engine import Engine
+
+from alignn.config import TrainingConfig
+
+
+def group_decay(model):
+    """Omit weight decay from bias and batchnorm params."""
+    decay, no_decay = [], []
+
+    for name, p in model.named_parameters():
+        if "bias" in name or "bn" in name or "norm" in name:
+            no_decay.append(p)
+        else:
+            decay.append(p)
+
+    return [
+        {"params": decay},
+        {"params": no_decay, "weight_decay": 0},
+    ]
+
+
+def setup_optimizer(params, config: TrainingConfig):
+    """Set up optimizer for param groups."""
+    if config.optimizer == "adamw":
+        optimizer = torch.optim.AdamW(
+            params,
+            lr=config.learning_rate,
+            weight_decay=config.weight_decay,
+        )
+    elif config.optimizer == "sgd":
+        optimizer = torch.optim.SGD(
+            params,
+            lr=config.learning_rate,
+            momentum=0.9,
+            weight_decay=config.weight_decay,
+        )
+    return optimizer
+
+
+def setup_evaluator_with_grad(
+    model,
+    metrics,
+    prepare_batch,
+    device="cpu",
+    non_blocking=False,
+    output_transform: Callable[[Any, Any, Any], Any] = lambda x, y, y_pred: (
+        y_pred,
+        y,
+    ),
+):
+    """Set up custom ignite evaluation engine.
+
+    Closely follows
+    https://github.com/pytorch/ignite/blob/master/ignite/engine/__init__.py#L558
+    with the exception of keeping gradient computation
+    """
+
+    def evaluation_step(engine, batch):
+        """Evaluate step, including gradient tracing."""
+        model.eval()
+        x, y = prepare_batch(batch, device=device, non_blocking=non_blocking)
+        y_pred = model(x)
+
+        return output_transform(x, y, y_pred)
+
+    evaluator = Engine(evaluation_step)
+    for name, metric in metrics.items():
+        metric.attach(evaluator, name)
+
+    return evaluator
