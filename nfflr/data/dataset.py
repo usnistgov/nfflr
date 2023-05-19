fix force field units

nfflr/data/dataset.py
@@ -155,7 +155,7 @@ class AtomsDataset(torch.utils.data.Dataset):
         # TODO: make sure datasets use standard units...
         # data store should have total energies in eV
         if self.energy_units == "eV/atom":
-            target["energy"] = target["energy"] / target["forces"].shape[0]
+            target["energy"] = target["energy"] * target["forces"].shape[0]
             # note: probably keep forces in eV/at and scale up predictions...
         elif self.energy_units == "eV":
             # data store should have total energies in eV, so do nothing

nfflr/models/gnn/alignn.py
@@ -12,7 +12,7 @@ import dgl
 import torch
 from torch import nn
 
-from dgl.nn import AvgPooling
+from dgl.nn import AvgPooling, SumPooling
 
 from nfflr.models.utils import (
     smooth_cutoff,
@@ -112,7 +112,10 @@ class ALIGNN(nn.Module):
             ]
         )
 
-        self.readout = AvgPooling()
+        if config.energy_units == "eV/atom":
+            self.readout = AvgPooling()
+        else:
+            self.readout = SumPooling()
 
         self.fc = nn.Linear(config.hidden_features, config.output_features)
 