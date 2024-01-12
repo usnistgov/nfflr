f0f0cfcb685adf486d53ea095f1d62bdd727996e
Brian DeCost <brian.decost@nist.gov>
2024-01-12 14:52:40 -0500

    finish moving namespaces: delete old imports

--- nfflr/nn/layers/__init__.py
+++ nfflr/nn/layers/__init__.py
@@ -1,11 +1,1 @@
 """Shared model-building components."""
-from .common import MLPLayer, FeedForward
-from .norm import Norm, InstanceNorm
-from .basis import RBFExpansion, ChebyshevExpansion
-from .conv import EdgeGatedGraphConv
-from .alignn import ALIGNNConv, SparseALIGNNConv
-from .atomfeatures import (
-    AttributeEmbedding,
-    AtomicNumberEmbedding,
-    PeriodicTableEmbedding,
-)
