c507377f2608667f5c02fbc25ed68f47f84e716c
Brian DeCost <brian.decost@nist.gov>
2024-02-06 17:06:33 -0500

    add target unscaling and logging to ase interface

--- nfflr/ase.py
+++ nfflr/ase.py
@@ -1,4 +1,5 @@
 __all__ = "NFFLrCalculator"
+from typing import Optional
 
 import torch
 from ase.calculators.calculator import Calculator, all_changes
@@ -9,9 +10,19 @@ import nfflr
 class NFFLrCalculator(Calculator):
     implemented_properties = ["energy", "forces", "stress"]
 
-    def __init__(self, model: torch.nn.Module, **kwargs):
+    def __init__(
+        self,
+        model: torch.nn.Module,
+        scaler: Optional[torch.nn.Module] = None,
+        log_history: bool = False,
+        **kwargs
+    ):
         super().__init__(**kwargs)
         self.model = model
+        self.scaler = scaler
+
+        self.log_history = log_history
+        self.history = []
 
     def calculate(self, atoms=None, properties=None, system_changes=all_changes):
 
@@ -21,8 +32,18 @@ class NFFLrCalculator(Calculator):
 
         outputs = self.model(nfflr.Atoms(atoms))
 
-        self.results["energy"] = outputs["total_energy"].detach().item()
-        self.results["forces"] = outputs["forces"].detach().numpy()
-        self.results["stress"] = (
-            outputs["stress"].detach().numpy().squeeze() / atoms.get_volume()
-        )
+        energy = outputs["total_energy"].detach()
+        forces = outputs["forces"].detach()
+
+        if self.scaler is not None:
+            energy = self.scaler.inverse_transform(energy)
+            forces = self.scaler.unscale(forces)
+
+        self.results["energy"] = energy.item()
+        self.results["forces"] = forces.numpy()
+        # self.results["stress"] = (
+        #     outputs["stress"].detach().numpy().squeeze() / atoms.get_volume()
+        # )
+
+        if self.log_history:
+            self.history.append(energy.item())
