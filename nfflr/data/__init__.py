04cff26294b86588745d5b2e490450035542e812
Brian DeCost <brian.decost@nist.gov>
2024-04-05 13:38:17 -0400

    fix vasprun.xml discovery and add convenience function to dump to json

--- nfflr/data/__init__.py
+++ nfflr/data/__init__.py
@@ -4,4 +4,4 @@ __all__ = ()
 
 from .datasets.mlearn import mlearn_dataset
 from .datasets.deepmd import deepmd_hea_dataset
-from .datasets.vasp import vasprun_dataset
+from .datasets.vasp import vasprun_dataset, vasprun_to_json
